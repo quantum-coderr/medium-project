f7d853743e2fd1dfb355ca13e48dcd5466e441d5
quantumcoderr <47424629+quantum-coderr@users.noreply.github.com>
2025-07-25 15:05:51 +0000

    project-complete

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,123 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { verify } from "hono/jwt";
+import { createPostInput, updatePostInput } from "@quantum-coderr/medium";
+
+export const blogRouter = new Hono<{
+    Bindings : {
+        DATABASE_URL : string,
+        JWT_SECRET : string,
+    },
+    Variables : {
+        userId : string
+    }
+}>();
+
+blogRouter.use('/*', async (c,next) => {
+    const authHeader = c.req.header("Authorization") || "";
+    try {
+        const user = await verify(authHeader, c.env.JWT_SECRET);
+        if(user){
+            c.set("userId", String(user.id));
+            await next();
+        } else {
+            c.status(403);
+            return c.json("invalid credentials");
+        }
+    } catch(e) {
+        c.json("wrong inp");
+    }
+});
+
+blogRouter.post('/', async (c) => {
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const authorId = c.get("userId");
+    const { success } = createPostInput.safeParse(body);
+	if (!success) {
+		c.status(400);
+		return c.json({ error: "invalid input" });
+	}
+    try {
+        const post = await prisma.post.create({
+            data: {
+                title: body.title,
+                content: body.content,
+                authorId : Number(authorId)
+            }
+        }); 
+
+        return c.json({id : post.id});
+    } catch(e){
+        c.json("issue with post");
+        c.status(403);
+    }
+});
+
+blogRouter.put('/', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const { success } = updatePostInput.safeParse(body);
+	if (!success) {
+		c.status(400);
+		return c.json({ error: "invalid input" });
+	}
+    try {
+        const post = await prisma.post.update({
+            where :{
+                id : body.id
+            },
+            data: {
+                title: body.title,
+                content: body.content,
+                authorId : 1
+            }
+        }); 
+        return c.json({id : post.id});
+    } catch(e){
+        c.status(403);
+    }
+});
+
+blogRouter.get('/bulk', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    try {
+        const post = await prisma.post.findMany({
+            
+        }); 
+
+        return c.json({post});
+    } catch(e){
+        c.status(403);
+    }
+});
+
+blogRouter.get('/:id', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+    const id = await c.req.param("id");
+    try {
+        const post = await prisma.post.findUnique({
+            where : {
+                id : Number(id)
+            }
+        }); 
+
+        return c.json({post : post});
+    } catch(e){
+        c.status(403);
+    }
+});
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.rohansingh.workers.dev"
